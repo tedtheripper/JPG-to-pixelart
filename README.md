ce875d42223c918044b13b1863653962d3f6656c
tedtheripper <lemon.juice0731@gmail.com>
2020-11-27 10:29:23 +0100

    small code refactoring

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # JPG to pixelart
-Short python program for creating awesome pixelarts from given jpg images.<br />
-The input jpg image must be present in the same folder as the script and be named "input.jpg"<br />
+Short python script for creating awesome pixelarts from given jpg/jpeg images.<br />
+The input jpg image can be present in the same folder as the script and be named "input.jpeg"<br />
+or user can specify the path to the image like this:<br />
+```
+python3 main.py ~/Pictures/examples/example.jpg
+```
 After launching script successfully, the result will be present in the same folder as "output.png"
 
 
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os, sys
 from PIL import Image
 
-path = 'input.jpg'
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = "input.jpeg"
+ext = path.split(".")
+if not ext[-1] == "jpg" and not ext[-1] == "jpeg":
+    print("Wrong input specified. (Image must be a jpg or jpeg)")
+    exit()
 path_final = 'output.png'
-multiplier = 8          # 'pixelization' ratio (higher resolution => higher ratio)
+multiplier = 8          # 'pixelization' ratio (higher resolution => higher ratio should be set)
 try:
     im = Image.open(path, 'r')
 except:
     print("No input specified.")
-    quit()
+    exit()
 pix_val = list(im.getdata())
-size_x = im.size[0]
-size_y = im.size[1]
+size_x, size_y = im.size[0], im.size[1]
+if multiplier > min(size_x, size_y)//8 or multiplier < 1:
+    print("Incorrect multiplier")
+    exit()
 im_f = Image.new(im.mode, (size_x, size_y))
 
-pix_n = []
-packer = 0
-avg_r = 0
-avg_g = 0
-avg_b = 0
-index = 0
+new_pixels = []
+packer, avg_r, avg_g, avg_b, index = 0, 0, 0, 0, 0
 for p in pix_val:       # horizontal packing
     index += 1
     avg_r += p[0]
@@ -28,37 +33,30 @@ for p in pix_val:       # horizontal packing
     packer += 1
     if packer == multiplier or index % size_x == 0:
         for i in range(0, packer):
-            pix_n.append([avg_r//packer, avg_g//packer, avg_b//packer])
-        avg_r = 0
-        avg_g = 0
-        avg_b = 0
-        packer = 0
-avg_r = 0
-avg_g = 0
-avg_b = 0
-index = 0
+            new_pixels.append([avg_r//packer, avg_g//packer, avg_b//packer])
+        avg_r, avg_g, avg_b, packer = 0, 0, 0, 0
+
+avg_r, avg_g, avg_b, index = 0, 0, 0, 0
 for i in range(0, size_x):      # vertical packing
     for h in range(0, size_y-1, multiplier):
         for j in range(0, multiplier):
             if h+j >= size_y:
                 break
-            avg_r += pix_n[(h+j)*size_x + i][0]
-            avg_g += pix_n[(h+j)*size_x + i][1]
-            avg_b += pix_n[(h+j)*size_x + i][2]
+            avg_r += new_pixels[(h+j)*size_x + i][0]
+            avg_g += new_pixels[(h+j)*size_x + i][1]
+            avg_b += new_pixels[(h+j)*size_x + i][2]
         for m in range(0, multiplier):
             if h+m >= size_y:
                 break
-            pix_n[(h+m)*size_x + i] = [avg_r//multiplier, avg_g//multiplier, avg_b//multiplier]
+            new_pixels[(h+m)*size_x + i] = [avg_r//multiplier, avg_g//multiplier, avg_b//multiplier]
         avg_r = 0
         avg_g = 0
         avg_b = 0
 
-list_of_pixels = []
-for x in pix_n:
-    list_of_pixels.append(tuple(x))
+list_of_pixels = [tuple(x) for x in new_pixels]
 im_f.putdata(list_of_pixels)
 try:
     im_f.save(path_final)
 except:
-    print("An error occured (saving).")
+    print("An error occured while saving.")
     quit()
